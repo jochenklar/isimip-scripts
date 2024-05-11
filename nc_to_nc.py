0a457ebf0a120d35afdb887de54de9f0b349480c
Jochen Klar <mail@jochenklar.de>
2024-05-11 18:53:53 +0200

    Update nc_to_nc.py

--- nc_to_nc.py
+++ nc_to_nc.py
@@ -1,9 +1,9 @@
 import numpy as np
 import xarray as xr
 
-# takes 5 minutes using xr.concat
-# takes 3 1/2 minutes with pre allocated ds w/o compression
-# takes 4 1/2 minutes with pre allocated ds with compression
+# takes 5 minutes using a previous approach using xr.concat
+# takes 3 1/2 minutes with pre-allocated ds w/o compression
+# takes 4 1/2 minutes with pre-allocated ds with compression
 
 rng = np.random.default_rng()
 
@@ -28,6 +28,7 @@ paths = [
     '~/data/isimip/datasets/ISIMIP3a/InputData/climate/atmosphere/obsclim/global/daily/historical/GSWP3-W5E5/gswp3-w5e5_obsclim_pr_global_daily_2011_2019.nc',
 ]
 
+# first pass: collect the time axis of all files
 var_name = None
 times = None
 for path in paths:
@@ -38,6 +39,7 @@ for path in paths:
         else:
             times = xr.concat([times, ds['time']], 'time')
 
+# allocate a data array to store the time series for each point
 var = xr.DataArray(
     dims=['idx', 'time'],
     coords={
@@ -45,6 +47,8 @@ var = xr.DataArray(
         'time': times
     })
 
+# second pass: loop over datasets and select the time series for each point and write it
+# into the allocated data array
 for path in paths:
     print(path)
 
@@ -53,6 +57,7 @@ for path in paths:
             ds_point = ds.sel(lon=lon, lat=lat, method='nearest')
             var.loc[idx, ds_point['time'][0]:ds_point['time'][-1]] = ds_point[var_name]
 
+# create a dataset with the data array and lon and lat as variables
 ds_points = xr.Dataset(
     data_vars={
         'lon': (['idx'], lons),
@@ -65,6 +70,6 @@ ds_points = xr.Dataset(
     }
 )
 
+# print and write the dataset
 print(ds_points)
-
 ds_points.to_netcdf('data/points.nc', encoding={var_name: {'zlib': True, 'complevel': 5}})
